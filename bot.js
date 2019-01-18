c7895c869945f7d65866a227868e30f798246b2c
hatemabdlas <45836024+hatemabdlas@users.noreply.github.com>
2019-01-18 09:10:00 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get(535613867401805850)
+setInterval(function() {
+channel.send(HATEM SPAM HERE GIVE MI CREDITS HAAAAA WTF I AM ABIG PRO);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
